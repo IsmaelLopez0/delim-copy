c0509f03180535cd3523e032561b310635024e3d
Ismael López <ismael97lopez@gmail.com>
2024-09-12 00:08:26 -0700

    Ajustes para despliegue en GitHub

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: 'https://comma-spt.github.io',
+  base: 'delim-copy',
 });
